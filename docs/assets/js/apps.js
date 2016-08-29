e150e01997d270655c6a628a4ec790409cbaa9ec
tomtoma1025 <tomtoma1025@gmail.com>
2016-08-29 17:38:10 +0900

    sample code
    sample code

--- docs/assets/js/apps.js
+++ docs/assets/js/apps.js
@@ -2,30 +2,30 @@ var $btn_scan = document.getElementById('scan_device'),
     $console = document.getElementById('console');
 
 $btn_scan.addEventListener('click', function(){
-  log('Requesting Bluetooth Device...');
+  console.log('Requesting Bluetooth Device...');
   navigator.bluetooth.requestDevice(
     {filters: [{services: ['battery_service']}]})
   .then(device => {
-    log('Connecting to GATT Server...');
+    console.log('Connecting to GATT Server...');
     return device.gatt.connect();
   })
   .then(server => {
-    log('Getting Battery Service...');
+    console.log('Getting Battery Service...');
     return server.getPrimaryService('battery_service');
   })
   .then(service => {
-    log('Getting Battery Level Characteristic...');
+    console.log('Getting Battery Level Characteristic...');
     return service.getCharacteristic('battery_level');
   })
   .then(characteristic => {
-    log('Reading Battery Level...');
+    console.log('Reading Battery Level...');
     return characteristic.readValue();
   })
   .then(value => {
     let batteryLevel = value.getUint8(0);
-    log('> Battery Level is ' + batteryLevel + '%');
+    console.log('> Battery Level is ' + batteryLevel + '%');
   })
   .catch(error => {
-    log('Argh! ' + error);
+    console.log('Argh! ' + error);
   });
 });
