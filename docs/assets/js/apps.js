75574e8d39657cd6af6caaa76294083735c877ac
tomtoma1025 <tomtoma1025@gmail.com>
2016-08-29 17:41:46 +0900

    sample code
    sample code

--- docs/assets/js/apps.js
+++ docs/assets/js/apps.js
@@ -24,6 +24,7 @@ $btn_scan.addEventListener('click', function(){
   .then(value => {
     let batteryLevel = value.getUint8(0);
     console.log('> Battery Level is ' + batteryLevel + '%');
+    $console.innerHtml = '電池残量は' + batteryLevel + '%！！！'
   })
   .catch(error => {
     console.log('Argh! ' + error);
